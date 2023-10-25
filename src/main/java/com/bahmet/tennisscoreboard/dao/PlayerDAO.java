09f82d4fb3149d6166bc9a7a0379996a865b3a06
Maxim Bagmet <quanty.zero@gmail.com>
2023-10-25 18:42:31 +0200

    feat: add dao for Player and Match model classes

---
+++ src/main/java/com/bahmet/tennisscoreboard/dao/PlayerDAO.java
@@ -0,0 +1,38 @@
+package com.bahmet.tennisscoreboard.dao;
+
+
+import com.bahmet.tennisscoreboard.exception.DatabaseException;
+import com.bahmet.tennisscoreboard.exception.PlayerNameAlreadyExistsException;
+import com.bahmet.tennisscoreboard.model.Player;
+import com.bahmet.tennisscoreboard.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.exception.ConstraintViolationException;
+
+public class PlayerDAO {
+    public Player findByName(String name) {
+        String HQLQuery = "From Player where name = :name";
+
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            return session.createQuery(HQLQuery, Player.class).setParameter("name", name).uniqueResult();
+        } catch (Exception e) {
+            throw new DatabaseException("Database error");
+        }
+    }
+
+    public Player save(Player player) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            session.persist(player);
+            transaction.commit();
+            return player;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+
+            throw new DatabaseException("Database error");
+        }
+    }
+}
