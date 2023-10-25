48a9e6a70dcd87bdf38f7e33445dd4b67e122e53
Maxim Bagmet <quanty.zero@gmail.com>
2023-10-25 18:44:04 +0200

    feat: add Validator

---
+++ src/main/java/com/bahmet/tennisscoreboard/util/Validator.java
@@ -0,0 +1,7 @@
+package com.bahmet.tennisscoreboard.util;
+
+public class Validator {
+//    public static boolean validatePlayerName(String playerName) {
+//        return playerName.matches()
+//    }
+}
