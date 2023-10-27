d4145864541992925af49524850c6311825c7863
Maxim Bagmet <quanty.zero@gmail.com>
2023-10-27 19:35:41 +0200

    chore: delete Validator

--- src/main/java/com/bahmet/tennisscoreboard/util/Validator.java
+++
@@ -1,7 +0,0 @@
-package com.bahmet.tennisscoreboard.util;
-
-public class Validator {
-//    public static boolean validatePlayerName(String playerName) {
-//        return playerName.matches()
-//    }
-}
